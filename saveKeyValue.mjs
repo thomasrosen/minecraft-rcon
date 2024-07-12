7fd886189b34dfcbce2e5fb75fcbece86efaeee7
thomasrosen <thomas.rosen@me.com>
2024-07-12 05:50:34 +0200

    Update saveKeyValue.mjs

--- saveKeyValue.mjs
+++ saveKeyValue.mjs
@@ -75,9 +75,6 @@ export async function saveKeyValue({ repoUrl = process.env.GIT_REPO_URL, commitM
     token: process.env.GIT_ACCESS_TOKEN
   };
 
-  console.log('process.env.GITHUB_TOKEN', process.env.GITHUB_TOKEN)
-  console.log('process.env.GIT_ACCESS_TOKEN', process.env.GIT_ACCESS_TOKEN)
-
   // Choose one of the auth methods
   const auth = authToken; // or authBasic
 
